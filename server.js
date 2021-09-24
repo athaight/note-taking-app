288edac5738b7c48f96b6f435f1e5df7d11ec95f
Andrew Haight <haight.haight@gmail.com>
2021-09-24 16:08:38 -0700

    added delete, refactored, rmv clog attempt

--- server.js
+++ server.js
@@ -4,45 +4,49 @@ const path = require('path');
 const fs = require('fs/promises');
 // const uuid = require('./helpers/uuid');
 const app = express();
-const PORT = process.env.PORT || 3333;
+const PORT = process.env.PORT || 3023;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-
-app.get('/', (req, res) =>
-res.sendFile(path.join(__dirname, '/public/index.html'))
-);
+// Retreives the home page
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "index.html")));
+// Retreives the notes page
 app.get("/notes", (req, res) =>
-  res.sendFile(path.join(__dirname, "/public/notes.html"))
+  res.sendFile(path.join(__dirname, "public/notes.html"))
 );
-
+// Retreives the note database and connects to notes page
 app.get("/api/notes", (req, res) =>
-  res.sendFile(path.join(__dirname, "/db/db.json"))
+  res.sendFile(path.join(__dirname, "db/db.json"))
 );
-
-app.get('/api/notes', (req, res) => {
-  res.status(200).json(`${req.method} request received to get reviews`);
-});
-
-app.post('/api/notes', async (req, res) => {
-  console.info(`${req.method} request received to add a note`);
-  const {title, text} = req.body;
+// Note is saved to database with randomized id 
+app.post("/api/notes", async (req, res) => {
   const dataToRead = await fs.readFile("./db/db.json", "utf8");
-
   const parsedData = JSON.parse(dataToRead);
-
-  parsedData.push(req.body);
+    parsedData.push({
+      ...req.body,
+      id: Math.floor((1 + Math.random()) * 0x10000)
+        .toString(16)
+        .toString(1),
+  });
 
   const stringifiedData = JSON.stringify(parsedData);
+  await fs.writeFile("./db/db.json", stringifiedData);
+  res.send();
+});
+// If a note is present with id and delete button is pressed note is removed from database 
+app.delete("/api/notes/:id", async (req, res) => {
+  const dataToRead = await fs.readFile("./db/db.json", "utf8");
+  const parsedData = JSON.parse(dataToRead);
+  const notes = parsedData.filter((note) => note.id !== req.params.id);
+  const stringifiedData = JSON.stringify(notes);
 
   await fs.writeFile("./db/db.json", stringifiedData);
-  res.send("whatever");
+  res.send("Deleted Note!");
 });
 
-
-
+// Projects to selected PORT (in this case PORT:3023)
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
